925ec5ea6b2a703bfb380ab5a82a92dc8d8197f6
siri3900 <thotakurasiri9999@gmail.com>
2025-07-23 11:34:31 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class TypeConversion {
+public static void main(String args[]) {
+	int n=100;
+	double d =n;
+	System.out.println("Integer value:"+n);
+	System.out.println("Converted to double: "+d);
+}
+}
